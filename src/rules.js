30e7b1ac8b605f7ff3d197c3c153b55826e76043
Mario Codarin <codarinmario@gmail.com>
2025-03-27 17:41:54 +0100

    fixed rule numbers

--- src/rules.js
+++ src/rules.js
@@ -1,162 +1,162 @@
 export const rules = [
   {
     id: 1,
-    title: "Se ci avessero insegnato di meno, avremmo imparato di più",
-    detail: "Regola 1: «Se ci avessero insegnato di meno, avremmo imparato di più». Questa regola sottolinea l'importanza di non sovraccaricare di informazioni, permettendo ai giocatori di sviluppare il proprio pensiero autonomo. L'iniziativa dei singoli è fondamentale in condizioni di libertà mentale."
+    title: "Regola 1. «Se ci avessero insegnato di meno, avremmo imparato di più»",
+    detail: "Questa regola sottolinea l'importanza di non sovraccaricare di informazioni, permettendo ai giocatori di sviluppare il proprio pensiero autonomo. L'iniziativa dei singoli è fondamentale in condizioni di libertà mentale."
   },
   {
     id: 2,
-    title: "Il calcio è semplice: devi fare l’opposto di quello che fanno i tuoi avversari",
-    detail: "Regola 2: «Il calcio è semplice: devi fare l’opposto di quello che fanno i tuoi avversari». Questa regola introduce la filosofia della semplicità nel calcio, dove lo scopo del gioco è passare la palla ai compagni con la soluzione più semplice."
+    title: "Regola 2. «Il calcio è semplice: devi fare l'opposto di quello che fanno i tuoi avversari»",
+    detail: "Questa regola introduce la filosofia della semplicità nel calcio, dove lo scopo del gioco è passare la palla ai compagni con la soluzione più semplice."
   },
   {
     id: 3,
-    title: "La semplicità è la cosa più difficile",
-    detail: "Regola 3: «La semplicità è la cosa più difficile». Questa regola evidenzia come la semplicità sia difficile da insegnare nel calcio, perché porta a comprendere concetti basilari. Più si scende di categoria, meno i giocatori passano la palla perché vogliono dimostrare le proprie abilità."
+    title: "Regola 3. «La semplicità è la cosa più difficile»",
+    detail: "Questa regola evidenzia come la semplicità sia difficile da insegnare nel calcio, perché porta a comprendere concetti basilari. Più si scende di categoria, meno i giocatori passano la palla perché vogliono dimostrare le proprie abilità."
   },
   {
     id: 4,
-    title: "Voglio giocatori pensanti e non polli d’allevamento",
-    detail: "Regola 4: «Voglio giocatori pensanti e non polli d’allevamento». Questa regola mette in luce l'importanza di avere giocatori che pensano con la propria testa e non si limitano ad eseguire schemi predefiniti. I giocatori devono essere in grado di ragionare autonomamente."
+    title: "Regola 4. «Voglio giocatori pensanti e non polli d'allevamento»",
+    detail: "Questa regola mette in luce l'importanza di avere giocatori che pensano con la propria testa e non si limitano ad eseguire schemi predefiniti. I giocatori devono essere in grado di ragionare autonomamente."
   },
   {
     id: 5,
-    title: "Autenticità, direzione e un pizzico di empatia. Ecco gli ingredienti perfetti per guadagnarsi la fiducia dei giocatori",
-    detail: "Regola 5: «Autenticità, direzione e un pizzico di empatia. Ecco gli ingredienti perfetti per guadagnarsi la fiducia dei giocatori». Questi sono gli ingredienti per guadagnarsi la fiducia dei giocatori. L'empatia permette ai giocatori di sentirsi compresi, l'autenticità li spinge ad applicarsi di più e il metodo fornisce loro una direzione."
+    title: "Regola 5. «Autenticità, direzione e un pizzico di empatia. Ecco gli ingredienti perfetti per guadagnarsi la fiducia dei giocatori»",
+    detail: "Questi sono gli ingredienti per guadagnarsi la fiducia dei giocatori. L'empatia permette ai giocatori di sentirsi compresi, l'autenticità li spinge ad applicarsi di più e il metodo fornisce loro una direzione."
   },
   {
     id: 6,
-    title: "Se vuoi sviluppare il talento, lascia esprimere i giovani in libertà",
-    detail: "Regola 6: «Se vuoi sviluppare il talento, lascia esprimere i giovani in libertà». I bambini devono sentirsi rispettati nella loro crescita e nel diritto di sbagliare. Questa regola enfatizza l'importanza di non mortificare la spensieratezza dei giovani."
+    title: "Regola 6. «Se vuoi sviluppare il talento, lascia esprimere i giovani in libertà»",
+    detail: "I bambini devono sentirsi rispettati nella loro crescita e nel diritto di sbagliare. Questa regola enfatizza l'importanza di non mortificare la spensieratezza dei giovani."
   },
   {
     id: 7,
-    title: "Lasciati guidare dalle tue sensazioni... ma non quando sei sotto stress",
-    detail: "Regola 7: «Lasciati guidare dalle tue sensazioni... ma non quando sei sotto stress». Questa regola si concentra sull'istinto ragionato, ovvero la capacità di seguire le proprie sensazioni, ma di saperle controllare in situazioni di stress."
+    title: "Regola 7. «Lasciati guidare dalle tue sensazioni... ma non quando sei sotto stress»",
+    detail: "Questa regola si concentra sull'istinto ragionato, ovvero la capacità di seguire le proprie sensazioni, ma di saperle controllare in situazioni di stress."
   },
   {
     id: 8,
-    title: "Il momento per vincere alla roulette è breve. Se non lo cogli, la sconfitta è quasi certa",
-    detail: "Regola 8: «Il momento per vincere alla roulette è breve. Se non lo cogli, la sconfitta è quasi certa». Bisogna cogliere il momento giusto per agire, altrimenti la sconfitta è quasi certa. Le squadre vincenti sanno riconoscere le situazioni favorevoli."
+    title: "Regola 8. «Il momento per vincere alla roulette è breve. Se non lo cogli, la sconfitta è quasi certa»",
+    detail: "Bisogna cogliere il momento giusto per agire, altrimenti la sconfitta è quasi certa. Le squadre vincenti sanno riconoscere le situazioni favorevoli."
   },
   {
     id: 9,
-    title: "Il pensiero positivo abbassa l’energia",
-    detail: "Regola 9: «Il pensiero positivo abbassa l’energia». Questa regola sottolinea che l'eccessivo ottimismo può portare a una diminuzione dell'energia e della concentrazione, e che è importante rimanere concentrati e determinati."
+    title: "Regola 9. «Il pensiero positivo abbassa l'energia»",
+    detail: "Questa regola sottolinea che l'eccessivo ottimismo può portare a una diminuzione dell'energia e della concentrazione, e che è importante rimanere concentrati e determinati."
   },
   {
     id: 10,
-    title: "Non esiste il mantenimento. Esiste solo il miglioramento continuo",
-    detail: "Regola 10: «Non esiste il mantenimento. Esiste solo il miglioramento continuo». Questa regola sottolinea l'importanza di non accontentarsi mai e di cercare sempre margini di miglioramento, anche dopo una vittoria. C'è sempre qualcosa che può andare meglio."
+    title: "Regola 10. «Non esiste il mantenimento. Esiste solo il miglioramento continuo»",
+    detail: "Questa regola sottolinea l'importanza di non accontentarsi mai e di cercare sempre margini di miglioramento, anche dopo una vittoria. C'è sempre qualcosa che può andare meglio."
   },
   {
     id: 11,
-    title: "Se vuoi innalzare i picchi di prestazione, usa il cazzeggio creativo",
-    detail: "Regola 11: «Se vuoi innalzare i picchi di prestazione, usa il cazzeggio creativo». Il \"cazzeggio\" è inteso come un modo per rilassarsi e recuperare l'ironia, utile per il benessere della squadra. Il cazzeggio è una filosofia di vita."
+    title: "Regola 11. «Se vuoi innalzare i picchi di prestazione, usa il cazzeggio creativo»",
+    detail: "Il \"cazzeggio\" è inteso come un modo per rilassarsi e recuperare l'ironia, utile per il benessere della squadra. Il cazzeggio è una filosofia di vita."
   },
   {
     id: 12,
-    title: "L’importanza di dire soltanto tre cose semplici ai giocatori",
-    detail: "Regola 12: «L’importanza di dire soltanto tre cose semplici ai giocatori». Questa regola evidenzia l'efficacia dei messaggi concisi e diretti, specialmente negli intervalli delle partite."
+    title: "Regola 12. «L'importanza di dire soltanto tre cose semplici ai giocatori»",
+    detail: "Questa regola evidenzia l'efficacia dei messaggi concisi e diretti, specialmente negli intervalli delle partite."
   },
   {
     id: 13,
-    title: "Per vincere un altro campionato dobbiamo subire meno gol di tutti",
-    detail: "Regola 13: «Per vincere un altro campionato dobbiamo subire meno gol di tutti». Questa regola sottolinea che una buona difesa è fondamentale per vincere un campionato."
+    title: "Regola 13. «Per vincere un altro campionato dobbiamo subire meno gol di tutti»",
+    detail: "Questa regola sottolinea che una buona difesa è fondamentale per vincere un campionato."
   },
   {
     id: 14,
-    title: "Essere autorevoli, non autoritari",
-    detail: "Regola 14: «Essere autorevoli, non autoritari». Questa regola spiega che un allenatore deve essere una figura di riferimento autorevole, ma senza diventare autoritario. L'allenatore si confronta con i ragazzi e le altre componenti del progetto."
+    title: "Regola 14. «Essere autorevoli, non autoritari»",
+    detail: "Questa regola spiega che un allenatore deve essere una figura di riferimento autorevole, ma senza diventare autoritario. L'allenatore si confronta con i ragazzi e le altre componenti del progetto."
   },
   {
     id: 15,
-    title: "Gli attori principali sono sempre i giocatori",
-    detail: "Regola 15: «Gli attori principali sono sempre i giocatori». Questa regola sottolinea l'importanza di riconoscere il valore dei giocatori e di avere fiducia in loro."
+    title: "Regola 15. «Gli attori principali sono sempre i giocatori»",
+    detail: "Questa regola sottolinea l'importanza di riconoscere il valore dei giocatori e di avere fiducia in loro."
   },
   {
     id: 16,
-    title: "La vita e il calcio sono simili: è tutta una questione di equilibrio",
-    detail: "Regola 16: «La vita e il calcio sono simili: è tutta una questione di equilibrio». Questa regola si concentra sull'equilibrio e la gestione delle aspettative, inteso come bilanciamento tra componenti tecniche, emotive e comportamentali."
+    title: "Regola 16. «La vita e il calcio sono simili: è tutta una questione di equilibrio»",
+    detail: "Questa regola si concentra sull'equilibrio e la gestione delle aspettative, inteso come bilanciamento tra componenti tecniche, emotive e comportamentali."
   },
   {
     id: 17,
-    title: "Dai sempre al giocatore quello che gli serve, non ciò che vuole",
-    detail: "Regola 17: «Dai sempre al giocatore quello che gli serve, non ciò che vuole». Questa regola riguarda la psicologia dei singoli, ovvero la capacità di capire cosa serve realmente a ciascun giocatore, piuttosto che assecondare ogni loro richiesta."
+    title: "Regola 17. «Dai sempre al giocatore quello che gli serve, non ciò che vuole»",
+    detail: "Questa regola riguarda la psicologia dei singoli, ovvero la capacità di capire cosa serve realmente a ciascun giocatore, piuttosto che assecondare ogni loro richiesta."
   },
   {
     id: 18,
-    title: "Così da manager aziendalista costruisco un team vincente",
-    detail: "Regola 18: «Così da manager aziendalista costruisco un team vincente». Questa regola si concentra sul livello di compiacenza di una squadra. Più una squadra si compiace delle proprie capacità, meno le esprimerà sul campo."
+    title: "Regola 18. «Così da manager aziendalista costruisco un team vincente»",
+    detail: "Questa regola si concentra sul livello di compiacenza di una squadra. Più una squadra si compiace delle proprie capacità, meno le esprimerà sul campo."
   },
   {
     id: 19,
-    title: "Più alto è il livello di compiacenza, più è bassa la possibilità di vincere",
-    detail: "Regola 19: «Più alto è il livello di compiacenza, più è bassa la possibilità di vincere». Questa regola sottolinea l'importanza di analizzare gli errori per capire come migliorare, poiché l'errore non si giudica, ma si descrive."
+    title: "Regola 19. «Più alto è il livello di compiacenza, più è bassa la possibilità di vincere»",
+    detail: "Questa regola sottolinea l'importanza di analizzare gli errori per capire come migliorare, poiché l'errore non si giudica, ma si descrive."
   },
   {
     id: 20,
-    title: "Scegli sempre collaboratori migliori di te rispetto a ciò che puoi delegare",
-    detail: "Regola 20: «Scegli sempre collaboratori migliori di te rispetto a ciò che puoi delegare». Questa regola sottolinea l'importanza di circondarsi di persone competenti e affidabili."
+    title: "Regola 20. «Scegli sempre collaboratori migliori di te rispetto a ciò che puoi delegare»",
+    detail: "Questa regola sottolinea l'importanza di circondarsi di persone competenti e affidabili."
   },
   {
     id: 21,
-    title: "Per vincere, gioca la partita nella tua mente. Se non ti soddisfa, giocala di nuovo",
-    detail: "Regola 21: «Per vincere, gioca la partita nella tua mente. Se non ti soddisfa, giocala di nuovo». Questa regola si concentra sul rigiocare mentalmente le partite, per prepararsi al meglio e correggere gli errori."
+    title: "Regola 21. «Per vincere, gioca la partita nella tua mente. Se non ti soddisfa, giocala di nuovo»",
+    detail: "Questa regola si concentra sul rigiocare mentalmente le partite, per prepararsi al meglio e correggere gli errori."
   },
   {
     id: 22,
-    title: "Il bravo manager è colui che porta soluzioni, non problemi",
-    detail: "Regola 22: «Il bravo manager è colui che porta soluzioni, non problemi». Questa regola spiega che un buon allenatore deve saper risolvere i problemi e trovare soluzioni."
+    title: "Regola 22. «Il bravo manager è colui che porta soluzioni, non problemi»",
+    detail: "Questa regola spiega che un buon allenatore deve saper risolvere i problemi e trovare soluzioni."
   },
   {
     id: 23,
-    title: "La squadra diventa responsabile quando sa accendere e spegnere l’interruttore",
-    detail: "Regola 23: «La squadra diventa responsabile quando sa accendere e spegnere l’interruttore». Questa regola si riferisce alla capacità di una squadra di gestire i ritmi di gioco, alternando momenti di riposo a picchi di prestazione."
+    title: "Regola 23. «La squadra diventa responsabile quando sa accendere e spegnere l’interruttore»",
+    detail: "Questa regola si riferisce alla capacità di una squadra di gestire i ritmi di gioco, alternando momenti di riposo a picchi di prestazione."
   },
   {
     id: 24,
-    title: "L’organizzazione deve essere al servizio della fantasia e dell’estro",
-    detail: "Regola 24: «L’organizzazione deve essere al servizio della fantasia e dell’estro». Questa regola riguarda la gestione dei ritmi di gioco, dove l'organizzazione deve coesistere con la libera espressione del talento."
+    title: "Regola 24. «L’organizzazione deve essere al servizio della fantasia e dell’estro»",
+    detail: "Questa regola riguarda la gestione dei ritmi di gioco, dove l'organizzazione deve coesistere con la libera espressione del talento."
   },
   {
     id: 25,
-    title: "Nel calcio o fai gol, o fai assist, o corri",
-    detail: "Regola 25: «Nel calcio o fai gol, o fai assist, o corri». Questa regola, ripresa da Dunga, sottolinea che un buon giocatore deve contribuire alla squadra attraverso gol, assist o impegno fisico."
+    title: "Regola 25. «Nel calcio o fai gol, o fai assist, o corri»",
+    detail: "Questa regola, ripresa da Dunga, sottolinea che un buon giocatore deve contribuire alla squadra attraverso gol, assist o impegno fisico."
   },
   {
     id: 26,
-    title: "Tutti possono giocare a pallone. Non tutti possono giocare a calcio",
-    detail: "Regola 26: «Tutti possono giocare a pallone. Non tutti possono giocare a calcio». Questa regola, ispirata da Cruijff, parla della bellezza del gioco e della differenza tra giocare a pallone e giocare a calcio. La tecnica vera del calcio è passare il pallone con un tocco, nel giusto tempo, con la giusta velocità e sul piede preferito del compagno di squadra."
+    title: "Regola 26. «Tutti possono giocare a pallone. Non tutti possono giocare a calcio»",
+    detail: "Questa regola, ispirata da Cruijff, parla della bellezza del gioco e della differenza tra giocare a pallone e giocare a calcio. La tecnica vera del calcio è passare il pallone con un tocco, nel giusto tempo, con la giusta velocità e sul piede preferito del compagno di squadra."
   },
   {
     id: 27,
-    title: "Se non puoi vincere, fai finire la partita mantenendo il possesso palla",
-    detail: "Regola 27: «Se non puoi vincere, fai finire la partita mantenendo il possesso palla». Questa regola sottolinea l'importanza di gestire la palla e le energie quando la vittoria non è più a portata di mano."
+    title: "Regola 27. «Se non puoi vincere, fai finire la partita mantenendo il possesso palla»",
+    detail: "Questa regola sottolinea l'importanza di gestire la palla e le energie quando la vittoria non è più a portata di mano."
   },
   {
     id: 28,
-    title: "Se pensi a un concetto di gioco nel modo giusto, lo puoi disegnare",
-    detail: "Regola 28: «Se pensi a un concetto di gioco nel modo giusto, lo puoi disegnare». Questa regola si riferisce alla capacità di immaginare il gioco in modo così chiaro da poterlo realizzare sul campo."
+    title: "Regola 28. «Se pensi a un concetto di gioco nel modo giusto, lo puoi disegnare»",
+    detail: "Questa regola si riferisce alla capacità di immaginare il gioco in modo così chiaro da poterlo realizzare sul campo."
   },
   {
     id: 29,
-    title: "Ci alleniamo per giocare nel modo in cui giochiamo",
-    detail: "Regola 29: «Ci alleniamo per giocare nel modo in cui giochiamo». Questa regola sottolinea l'importanza di allenarsi in modo coerente con lo stile di gioco che si vuole adottare."
+    title: "Regola 29. «Ci alleniamo per giocare nel modo in cui giochiamo»",
+    detail: "Questa regola sottolinea l'importanza di allenarsi in modo coerente con lo stile di gioco che si vuole adottare."
   },
   {
     id: 30,
-    title: "Gli atleti sono come cavalli da corsa. Ogni tanto vanno messi al prato",
-    detail: "Regola 30: «Gli atleti sono come cavalli da corsa. Ogni tanto vanno messi al prato». Questa regola si basa sul concetto che gli atleti, come i cavalli da corsa, hanno bisogno di periodi di riposo per recuperare energie."
+    title: "Regola 30. «Gli atleti sono come cavalli da corsa. Ogni tanto vanno messi al prato»",
+    detail: "Questa regola si basa sul concetto che gli atleti, come i cavalli da corsa, hanno bisogno di periodi di riposo per recuperare energie."
   },
   {
     id: 31,
-    title: "L’attenzione sui dati porta chiarezza, mentre le suggestioni danno energia",
-    detail: "Regola 31: «L’attenzione sui dati porta chiarezza, mentre le suggestioni danno energia». Questa regola evidenzia come un allenatore debba alternare l'uso di dati oggettivi e suggestioni per comunicare efficacemente con la squadra."
+    title: "Regola 31. «L’attenzione sui dati porta chiarezza, mentre le suggestioni danno energia»",
+    detail: "Questa regola evidenzia come un allenatore debba alternare l'uso di dati oggettivi e suggestioni per comunicare efficacemente con la squadra."
   },
   {
     id: 32,
-    title: "L’emozione del campo dà energia, l’analisi razionale ti fa decidere",
-    detail: "Regola 32: «L’emozione del campo dà energia, l’analisi razionale ti fa decidere». Questa regola sottolinea l'importanza di saper equilibrare le emozioni del campo con l'analisi razionale per prendere decisioni efficaci."
+    title: "Regola 32. «L’emozione del campo dà energia, l’analisi razionale ti fa decidere»",
+    detail: "Questa regola sottolinea l'importanza di saper equilibrare le emozioni del campo con l'analisi razionale per prendere decisioni efficaci."
   }
 ];
